e4aed0ec184e4e68addab9c965a50f1bb4af0ede
Gerrald Mitchell <bluelionjr@gmail.com>
2017-03-11 18:53:34 -0500

    4 List Messages Complete

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,6 +1,7 @@
 (function() {
-    function HomeCtrl(Room, $uibModal) {
+    function HomeCtrl(Room, Message, $uibModal) {
         this.rooms = Room.all;
+        this.currentRoom = null;
         this.newRoom = function(roomName) {
             Room.newRoom(roomName);
         }
@@ -10,10 +11,15 @@
                 templateUrl: '/templates/modal.html',
                 controller: 'ModalCtrl as modal'
             });
-        }   
+        }
+        
+        this.setCurrentRoom = function (room) {
+            this.currentRoom = room;
+            this.messages = Message.getByRoomId(this.currentRoom.$id);
+        }
     }
     
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Message', '$uibModal', HomeCtrl]);
 })();
